b271e43ef22ca8a4878c250d99233275379093ff
Mohammad Rahman <mohammadrahman@Mohammads-MacBook-Air-2.local>
2024-02-15 13:46:25 -0500

    Initial Commit

---
+++ challenges/code/challenge_one.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int numOfStudents, numOfSubjects;
+
+    cout << "Enter the number of students and the number of subjects: ";
+    cin >> numOfStudents >> numOfSubjects;
+
+    for (int i; i < numOfStudents; i++) {
+        cout << "Grade: ";
+        double grade;
+
+        cin >> grade;
+
+        
+    }
+
+
+
+    return 0;
+}
